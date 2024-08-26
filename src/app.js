5093d19b2410e17183eb73c27e4dc4b81bd88e24
Anajerar <aurelio.najerar@gmail.com>
2024-08-26 17:20:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let Subject = who[Math.floor(Math.random() * 4)];
+  let verb = action[Math.floor(Math.random() * 4)];
+  let target = what[Math.floor(Math.random() * 3)];
+  let time = when[Math.floor(Math.random() * 5)];
+  console.log(Subject + " " + verb + " " + target + " " + time);
+  document.querySelector("#Excuse").innerHTML =
+    Subject + " " + verb + " " + target + " " + time;
 };
